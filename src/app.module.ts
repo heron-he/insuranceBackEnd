f8199d41e922181e965280a443745d78112f05fb
何某的剑 <749348403@qq.com>
2025-06-15 21:11:33 +0800

    更新依赖版本，调整数据库配置，修改默认数据库名为insurance，密码字段默认为空

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,8 +25,8 @@ import envConfig from '../config/env';
                 host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
                 port: configService.get<number>('DB_PORT', 3306), // 端口号
                 username: configService.get('DB_USER', 'root'), // 用户名
-                password: configService.get('DB_PASSWORD', 'root'), // 密码
-                database: configService.get('DB_DATABASE', 'blog'), //数据库名
+                password: configService.get('DB_PASSWORD', ''), // 密码
+                database: configService.get('DB_DATABASE', 'insurance'), //数据库名
                 timezone: '+08:00', //服务器上配置的时区
                 synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
                 entities: [__dirname + '/**/*.entity{.ts,.js}'], //实体文件
